84447ec9dbf6281d9fb15b02ee0e1e421e42dcbb
Rishi-Gehani <rishigehani18@gmail.com>
2025-02-22 21:47:08 +0530

    tried sessions

--- AWP Project/bookingSuccess.aspx.cs
+++ AWP Project/bookingSuccess.aspx.cs
@@ -11,7 +11,7 @@ namespace AWP_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void btnHome_Click(object sender, EventArgs e)
